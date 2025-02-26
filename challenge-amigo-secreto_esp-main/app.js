6d0849288a71fbc2a14e7958e1ba1df932e181f4
Stephanie E. Bingham Hernandez <binghamstephy@gmail.com>
2025-02-25 20:23:10 -0500

    Cambios del archivo de origen

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,11 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos =[];
+function ingresaramigos (elemento,texto)
+{
+    let elementoHTML = document.querySelectorAll(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+function limpiar (){
+    document.querySelector('#valorUsuario').value = '';
+}
